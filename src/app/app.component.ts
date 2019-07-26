113860a06eb4900e6a5c01e1e84be2c257dac1a7
Kern Zhao <siyangkernzhao@gmail.com>
2019-07-26 12:15:18 -0400

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,30 @@
 import {Component} from '@angular/core';
-import {ThemeService} from './theme.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <h1>angular-dynamic-theme-example</h1>
+
+    <div class="block primary-background">Primary</div>  <br>
+    <div class="block secondary-background">Secondary</div>  <br>
+
+    Set Primary Color: <input type="text" [(ngModel)]="primaryColor"> <br>
+    Set Secondary Color: <input type="text" [(ngModel)]="secondaryColor"> <br>
+
+    <button (click)="changeTheme(primaryColor, secondaryColor)">Save</button>
+  `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   primaryColor: string;
   secondaryColor: string;
 
-  constructor(public themeService: ThemeService) {
-    themeService.changeTheme('red', 'yellow');
+  constructor() {
+    this.changeTheme('red', 'yellow'); // Set default theme
+  }
+
+  changeTheme(primary: string, secondary: string) {
+    document.documentElement.style.setProperty('--primary-color', primary);
+    document.documentElement.style.setProperty('--secondary-color', secondary);
   }
 }
